d1a6b531b472c6a0538d02702241b10a3b14985c
atulya1028 <atulyaagrawal1996@gmail.com>
2025-09-19 16:25:24 +0530

    changes in server url

--- server.js
+++ server.js
@@ -18,11 +18,14 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "https://www.aaritjewels.com"],
+    "https://www.aaritjewels.com",
+    "https://aaritjewels.com"
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
+
 
 // Body parsing middleware
 app.use(express.json({ limit: '10mb' }));
