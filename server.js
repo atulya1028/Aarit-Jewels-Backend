6bf7f662f5a220ebf6bbfa2a9ec9906ab1f1d240
atulya1028 <atulyaagrawal1996@gmail.com>
2025-09-19 16:00:10 +0530

    url change

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 
 // Apply CORS middleware first
 app.use(cors({
-  origin: ['http://localhost:5173','https://aarit-jewels-frontend.vercel.app'],
+  origin: ["https://www.aaritjewels.com"],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
